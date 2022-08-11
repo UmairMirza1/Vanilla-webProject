b1dc62cfdfab741ad07c1a30c5727d20aa8b8c37
UmairMirza1 <umair3215@gmail.com>
2022-08-12 01:10:40 +0500

    Trying to work out login sign up

--- client.js
+++ client.js
@@ -13,9 +13,9 @@
         const maindiv = document.getElementById("maindiv");
         const welcomeview = document.getElementById("welcomeView");
         const clientView = document.getElementById("clientView");
+       // maindiv.innerHTML = welcomeview.innerHTML;
         
-        maindiv.innerHTML = welcomeview.innerHTML;
-        
+
 
 
 
@@ -33,7 +33,7 @@
         const email = loginform.email.value;
         const password = loginform.password.value;
         const error = document.getElementById("loginerror");
-
+        const userprofile= document.getElementById("--");
 
         console.log("im here");
         if (password.length < 6) {
@@ -46,8 +46,12 @@
           error.innerText(response.message);
         }
         else {
-            
-
+          
+          // user ko login krana hai idher 
+          window.localStorage.setItem('token', response.data);
+          maindiv.innerHTML=us
+          
+          
         }
 
       }
@@ -58,23 +62,49 @@
 
         const signupform = document.getElementById("signupform");
 
-        var username = signupform.username.value;
+        var email = signupform.username.value;
+        var firstname = signupform.firstname.value;
+        var familyname = signupform.familyname.value;
+
+        var city = signupform.city.value;
+
         const password = signupform.password.value;
         const repeatPassword = signupform.repeatPassword.value;
 
 
         var gender = signupform.gender.value;
         var country = signupform.country.value;
+        const error = document.getElementById("loginerror");
+
         if (password.localcompare(repeatPassword) != 0) {
-          // error message
+          
+          error.innerText("Passwords Do not match");
+
+          
         }
         if (password.value.length < 8) {
-          // error message 
+
+          error.innerText("Password length must be greater than zero");
         }
 
 
         signupform.addEventListener('submit', (e) => {
           e.preventDefault();
+          
+          var user = {
+            'email': email,
+            'password': password,
+            'firstname': firstname,
+            'familyname': familyname,
+            'gender': gender,
+            'city': city,
+            'country': country,
+            'messages': []
+          };
+          const response = serverstub.signUp(user);
+
+
+          
         })
 
 
@@ -86,14 +116,14 @@
         const userprofilediv = document.getElementById("ProfileDisplay");
         switch(tab)
         {
-          case home:
+          case 'home':
           userprofilediv.innerHTML=  test1.innerHTML;
 
             break;
-            case browse:
+            case 'browse':
               userprofilediv.innerHTML=  test2.innerHTML;
               break;
-              case settings:
+              case 'settings':
                 userprofilediv.innerHTML=  test3.innerHTML;
                 break;
                 
