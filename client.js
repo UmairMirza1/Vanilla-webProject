0f54ea66f8dda607b665c6ee463ec322ef646cd7
UmairMirza1 <umair3215@gmail.com>
2022-09-01 15:49:28 +0500

    merged single file and Js fixes

--- client.js
+++ client.js
@@ -1,146 +1,122 @@
-      displayView = function () {
+displayView = function () {
+  // the code to display view
+};
 
-        // the code to display view
+window.onload = function () {
+  const maindiv = document.getElementById("maindiv");
+  const welcomeview = document.getElementById("welcomeView");
+  const clientView = document.getElementById("clientView");
+  maindiv.innerHTML = welcomeview.innerHTML;
+};
 
+onsignin = function () {
+  const email = document.getElementById("signinEmail").value;
+  const password = document.getElementById("signinPassword").value;
+  const error = document.getElementById("loginError");
+  const clientView = document.getElementById("clientView");
+  const maindiv = document.getElementById("maindiv");
+  console.log("im here");
 
-      }
+  const response = serverstub.signIn(email, password);
 
+  console.log(response);
 
+  if (!response.success) {
+    error.innerText = response.message;
+  } else {
+    localStorage.setItem("token", response.data);
 
-      window.onload = function () {
+    maindiv.innerHTML = clientView.innerHTML;
+  }
+};
 
+onsignup = function () {
+  console.log("click");
+  //        const formData = new FormData(form);
+  // for (var pair of formData.entries()) {
+  //   console.log(pair[0] + ", " + pair[1]);
+  // }
+  const firstname = document.getElementById("firstName").value;
+  const familyname = document.getElementById("familyName").value;
+  const email = document.getElementById("signupEmail").value;
+  const password = document.getElementById("signupPassword").value;
+  const repeatPassword = document.getElementById("repeatPassword").value;
+  const gender = document.getElementById("gender").value;
+  const city = document.getElementById("city").value;
+  const country = document.getElementById("country").value;
 
-        const maindiv = document.getElementById("maindiv");
-        const welcomeview = document.getElementById("welcomeView");
-        const clientView = document.getElementById("clientView");
-       // maindiv.innerHTML = welcomeview.innerHTML;
-        
+  console.log(firstname, familyname, email, password, repeatPassword);
 
+  const error = document.getElementById("signuperror");
 
+  if (password !== repeatPassword) {
+    // error.innerText("Passwords Do not match");
+    console.log("no");
+    return;
+  }
 
+  if (password.length < 8) {
+    error.innerText = "Password length must be greater than eight";
 
+    console.log("nooo");
+    return;
+  }
 
-      }
+  const user = {
+    email,
+    password,
+    firstname,
+    familyname,
+    gender,
+    city,
+    country,
+  };
 
+  console.log(user);
 
+  const response = serverstub.signUp(user);
 
-      onsignin = function (e) {
+  console.log(response);
+  if (!response.success) {
+    error.innerText = response.message;
+  } else {
+    window.alert("you can login now ");
+  }
+};
 
-        e.preventDefault();
-        var token;
-        
-        const loginform = document.getElementById("loginform");
-        const email = loginform.email.value;
-        const password = loginform.password.value;
-        const error = document.getElementById("loginerror");
-        const userprofile= document.getElementById("--");
+//document.getElementById("home").addEventListener("click", display);
 
-        console.log("im here");
-        if (password.length < 6) {
+clickTab = function (tab) {
+  const userprofilediv = document.getElementById("ProfileDisplay");
+  // const test1 = document.getElementById("test1");
+  // const test2 = document.getElementById("test2");
+  // const test3 = document.getElementById("test3");
 
-          error.innerText = ('password must be greater than or equal to 6 characters');
-        }
+  const home = document.getElementById("hometab");
+  const browse = document.getElementById("browsetab");
+  const settings = document.getElementById("settingstab");
 
-        const response = serverstub.signIn(email, password);
-        if (!response.success) {
-          error.innerText(response.message);
-        }
-        else {
-          
-          // user ko login krana hai idher 
-          window.localStorage.setItem('token', response.data);
+  switch (tab) {
+    case "home":
+      //  userprofilediv.innerHTML=  test1.innerHTML;
+      home.style.display = "block";
+      settings.style.display = "none";
+      browse.style.display = "none";
 
+      break;
+    case "browse":
+      // userprofilediv.innerHTML=  test2.innerHTML;
+      browse.style.display = "block";
+      home.style.display = "none";
+      settings.style.display = "none";
 
-          maindiv.innerHTML=userprofile.innerHTML;
-          
-          
-        }
+      break;
+    case "settings":
+      //  userprofilediv.innerHTML=  test3.innerHTML;
 
-      }
-
-
-
-      onsignup = function () {
-
-        const signupform = document.getElementById("signupform");
-
-        var email = signupform.username.value;
-        var firstname = signupform.firstname.value;
-        var familyname = signupform.familyname.value;
-
-        var city = signupform.city.value;
-
-        const password = signupform.password.value;
-        const repeatPassword = signupform.repeatPassword.value;
-
-
-        var gender = signupform.gender.value;
-        var country = signupform.country.value;
-        const error = document.getElementById("loginerror");
-
-        if (password.localcompare(repeatPassword) != 0) {
-          
-          error.innerText("Passwords Do not match");
-
-          
-        }
-        if (password.value.length < 8) {
-
-          error.innerText("Password length must be greater than zero");
-        }
-
-
-        signupform.addEventListener('submit', (e) => {
-          e.preventDefault();
-          
-          var user = {
-            'email': email,
-            'password': password,
-            'firstname': firstname,
-            'familyname': familyname,
-            'gender': gender,
-            'city': city,
-            'country': country,
-            'messages': []
-          };
-          const response = serverstub.signUp(user);
-
-          if(!response.success){
-
-            error.innerText(response.message);
-          }
-          else{
-            window.alert("you can login now ")
-          }
-
-
-          
-        })
-
-
-      }
-
-      //document.getElementById("home").addEventListener("click", display);
-      
-      clickTab = function(tab){
-        const userprofilediv = document.getElementById("ProfileDisplay");
-        switch(tab)
-        {
-          case 'home':
-          userprofilediv.innerHTML=  test1.innerHTML;
-
-            break;
-            case 'browse':
-              userprofilediv.innerHTML=  test2.innerHTML;
-              break;
-              case 'settings':
-                userprofilediv.innerHTML=  test3.innerHTML;
-                break;
-                
-        }
-
-
-        
-
-
-      }
+      settings.style.display = "block";
+      browse.style.display = "none";
+      home.style.display = "none";
+      break;
+  }
+};
